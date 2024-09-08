dc4c20787c9569054163b3e286ded6defb4d2256
Dmytro Korostelyov <m0501543116@gmail.com>
2024-09-08 09:19:43 +0300

    Init1

--- HibernateMavenProject/src/main/java/org/example/Main.java
+++ HibernateMavenProject/src/main/java/org/example/Main.java
@@ -1,11 +1,30 @@
 package org.example;
 
 import org.example.dao.UserDAO;
+import org.example.entities.User;
 
 import javax.swing.*;
+import java.time.LocalDateTime;
+
 
 public class  Main {
    public static void main(String[] args) {
       UserDAO userDAO = new UserDAO();
+      User user = new User(0,"SA","NA", User.Gender.FEMALE,
+              LocalDateTime.now());
+      //
+      Integer save = userDAO.save(user);
+      System.out.println(save);
+      //
+      User byId = userDAO.findById(save);
+      System.out.println(byId);
+      //
+      user.setId(0);
+      System.out.println(user);
+      User saveMerge = userDAO.saveMerge(user);
+      System.out.println(user);
+      System.out.println(saveMerge);
+
+
    }
 }
--- HibernateMavenProject/src/main/java/org/example/dao/UserDAO.java
+++ HibernateMavenProject/src/main/java/org/example/dao/UserDAO.java
@@ -1,7 +1,9 @@
 package org.example.dao;
 
-import org.example.User;
+import org.example.entities.User;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 
 
@@ -10,8 +12,47 @@ public class UserDAO {
     private SessionFactory factory;
 
     public UserDAO() {
-       this.factory = new Configuration().configure("hibernate.cfg.xml")
-               //.addAnnotatedClass(User.class)
-               .buildSessionFactory();
+        this.factory = new Configuration().configure("hibernate.cfg.xml")
+                //.addAnnotatedClass(User.class)
+                .buildSessionFactory();
+    }
+
+        public Integer save(User user){
+        Session session = factory.getCurrentSession();
+        Integer saved = null;
+            try {
+                Transaction transaction = session.beginTransaction();
+                saved = (Integer) session.save(user);
+                transaction.commit();
+            } finally {
+                session.close();
+            }
+            return saved;
+    }
+
+    public User saveMerge(User user){
+        Session session = factory.getCurrentSession();
+        User saved = null;
+        try {
+            Transaction transaction = session.beginTransaction();
+            saved = session.merge(user);
+            transaction.commit();
+        } finally {
+            session.close();
+        }
+        return saved;
+    }
+
+    public User findById(Integer id){
+        Session session = factory.getCurrentSession();
+        User user = null;
+        try{
+            session.beginTransaction();
+            user = session.get(User.class, id);
+            session.getTransaction().commit();
+        }finally {
+            session.close();
+        }
+        return user;
     }
 }
